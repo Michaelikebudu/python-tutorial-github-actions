5473520c85133b440c7170e0cba6ac9471567111
Michael Ikebudu <michael.ikebudu@mycit.ie>
2020-11-30 14:48:20 +0000

    trying gitactions without pytest

--- gitactions_trial.py
+++ gitactions_trial.py
@@ -1,5 +1,18 @@
 print("Hi")
 
+import datetime
+
+
+def trial():
+    printw("Hello world!")
+    word = "Hello world!"
+    now = datetime.datetime.now()
+    printw("Current date and time is ")
+    printw(now.strftime("%A, %d-%m-%Y : %H:%M"))
+
+
+
+
 
 def printhello():
     return "Hello_world";
